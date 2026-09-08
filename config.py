2653cb824fa58520609e1883eb61f41381fb953a
algiiip <alghi0234@gmail.com>
2026-09-08 11:15:55 +0700

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ import os
 
 MYSQL_HOST = os.environ.get("MYSQL_HOST", "gateway01.ap-southeast-1.prod.aws.tidbcloud.com")
 MYSQL_USER = os.environ.get("MYSQL_USER", "3BysfHNMoeXfMGT.root")
-MYSQL_PASSWORD = os.environ.get("MYSQL_PASSWORD", "C4SLr9NnGd6zmG1U")
+MYSQL_PASSWORD = os.environ.get("MYSQL_PASSWORD", "Ls1nCvLlmKYdmVpa")
 MYSQL_DATABASE = os.environ.get("MYSQL_DATABASE", "invanteris_it")
 MYSQL_PORT = int(os.environ.get("MYSQL_PORT", 4000))
