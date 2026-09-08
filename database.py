5355f2d08d548fe28f379ec0b84676ad87f196d4
algiiip <alghi0234@gmail.com>
2026-09-08 11:27:59 +0700

    Update database.py

--- database.py
+++ database.py
@@ -5,7 +5,7 @@ def get_connection():
     return mysql.connector.connect(
         host=os.environ.get('MYSQL_HOST', 'gateway01.ap-southeast-1.prod.aws.tidbcloud.com'),
         user=os.environ.get('MYSQL_USER', '3BysfHNMoeXfMGT.root'),
-        password=os.environ.get('MYSQL_PASSWORD', 'hjNsHMmU73Os3jkl'),
+        password=os.environ.get('MYSQL_PASSWORD', 'kvTKZjIs874SHJVo'),
         database=os.environ.get('MYSQL_DATABASE', 'invanteris_it'),
         port=int(os.environ.get('MYSQL_PORT', 4000)),
         ssl_disabled=False
